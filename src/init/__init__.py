b9a8f07fa50d670958343cfa714a6bbf6720d4cf
Bwapt <bjlasbl44@gmail.com>
2023-10-06 14:58:29 +0000

    Complete restart from nothing with discord.py

--- src/init/__init__.py
+++ src/init/__init__.py
@@ -1,8 +1,2 @@
-#!/usr/bin/env python3
-#pylint: skip-file
 
-from lightbulb import BotApp
-from init.config.tokens import *
-
-def bot_init():
-    return BotApp(token=DISCORD_TOKEN, default_enabled_guilds=DISCORD_ID)
+    
